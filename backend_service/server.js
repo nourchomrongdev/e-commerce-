2658e99a32446e9362049b20af079647526b8c9d
nourchomrong <nourchomrong.biu@gmail.com>
2026-08-19 18:38:04 +0700

    payment pages

--- backend_service/server.js
+++ backend_service/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", apiRoutes);
 
 app.get("/", (req, res) => {
   res.json({
